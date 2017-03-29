9287c583aa04000c6783474ac196329754f1247c
mawujun1234 <mawujun1234@163.com>
2017-03-29 22:22:47 +0800

    111

--- platforms/android/assets/www/cordova_plugins.js
+++ platforms/android/assets/www/cordova_plugins.js
@@ -12,8 +12,8 @@ module.exports = [
 module.exports.metadata = 
 // TOP OF METADATA
 {
-    "cordova-plugin-whitelist": "1.3.1",
     "cordova-plugin-compat": "1.1.0",
+    "cordova-plugin-whitelist": "1.3.1",
     "phonegap-plugin-barcodescanner": "6.0.6"
 };
 // BOTTOM OF METADATA
